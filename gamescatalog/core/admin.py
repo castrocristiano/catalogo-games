7f555d750bcef423e59e3cf0a6d47b4b3528671f
Cristiano da Silva Castro <cscastro87@gmail.com>
2020-07-11 22:19:51 -0300

    Criando relações e entidades

--- gamescatalog/core/admin.py
+++ gamescatalog/core/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 
-from core.model.game import Jogo, Plataforma, PlataformaJogo
+from core.model.jogo import Jogo
+from core.model.plataforma import Plataforma
+from core.model.plataforma_jogo import PlataformaJogo
 
-admin.site.register(Jogo)
-admin.site.register(Plataforma)
-admin.site.register(PlataformaJogo)
+
+class JogoAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'slug', 'id']
+    prepopulated_fields = {'slug': ['titulo']}
+
+
+class PlataformaAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'slug', 'id']
+    prepopulated_fields = {'slug': ['titulo']}
+
+
+class PlataformaJogoAdmin(admin.ModelAdmin):
+    list_display = ['plataforma', 'jogo', 'slug']
+    prepopulated_fields = {'slug': ['plataforma', 'jogo']}
+
+
+admin.site.register(Jogo, JogoAdmin)
+admin.site.register(Plataforma, PlataformaAdmin)
+admin.site.register(PlataformaJogo, PlataformaJogoAdmin)
